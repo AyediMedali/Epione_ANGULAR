6e8c5ff8f2223b52566a779257ab352c113f1528
molka <molka.ouaja@esprit.tn>
2018-12-21 18:22:04 +0100

    molka commit ( redirection login )

--- Epione-ANGULAR/src/app/login/login-admin/login-admin.component.ts
+++ Epione-ANGULAR/src/app/login/login-admin/login-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-admin',
@@ -9,7 +10,7 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class LoginAdminComponent implements OnInit {
 
-  constructor(private fb:FormBuilder , private userService:UserService) { }
+  constructor(private fb:FormBuilder , private userService:UserService, private router : Router) { }
 
   form = this.fb.group({
     email : ['' ] ,
@@ -24,6 +25,7 @@ export class LoginAdminComponent implements OnInit {
     let password = this.form.get('password').value;
     this.userService.LoginAdmin(email,password).subscribe( 
       (Data) => {
+        this.router.navigate(['home']) ;
         console.log(Data);
       }
     )
--- Epione-ANGULAR/src/app/login/login-patient/login-patient.component.ts
+++ Epione-ANGULAR/src/app/login/login-patient/login-patient.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-patient',
@@ -9,7 +10,7 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class LoginPatientComponent implements OnInit {
 
-  constructor(private fb:FormBuilder , private userService:UserService) { }
+  constructor(private fb:FormBuilder , private userService:UserService, private router : Router) { }
 
   form = this.fb.group({
     email : ['' ] ,
@@ -26,6 +27,7 @@ export class LoginPatientComponent implements OnInit {
     let password = this.form.get('password').value;
     this.userService.LoginPatient(email,password).subscribe( 
       (Data) => {
+        this.router.navigate(['home']) ;
         console.log(Data);
       }
     )
